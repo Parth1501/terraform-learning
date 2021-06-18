9a7d786fff60469d8e7428f8eea2306b192adda7
Parth Shethwala <37747371+Parth1501@users.noreply.github.com>
2021-06-18 15:03:05 +0530

    Update main.tf

--- demo3/main.tf
+++ demo3/main.tf
@@ -1,21 +1,23 @@
-
 terraform {
-  required_provide {
-    source="hashicorp/aws"
-    version="~>3.27"
-    
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
   }
-  required_version=">=0.14.9"
+
+  required_version = ">= 0.14.9"
 }
 
 provider "aws" {
-  profile="default"
-  region="us-wes-2"
+  profile = "default"
+  region  = "us-west-2"
 }
 
-resource "aws_instance" "prs_server" {
-  ami="ami-0bac6fc47ad07c5f5"
-  instance_type="t2.micro"
+resource "aws_instance" "app_server" {
+  ami           = "ami-08d70e59c07c61a3a"
+  instance_type = "t2.micro"
+
   tags = {
     Name = var.instance_name
   }
