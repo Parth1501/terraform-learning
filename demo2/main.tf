b49e8fa9106580afc469911a5ea8f6911b885897
Parth Shethwala <37747371+Parth1501@users.noreply.github.com>
2021-06-18 14:49:40 +0530

    Create main.tf

---
+++ demo2/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_provide {
+    source="hashicorp/aws"
+    version="~>3.27"
+    
+  }
+  required_version=">=0.14.9"
+}
+
+provider "aws" {
+  profile="default"
+  region="us-wes-2"
+}
+
+resource "aws_instance" "prs_server" {
+  ami="ami-0bac6fc47ad07c5f5"
+  instance_type="t2.micro"
+  tags = {
+    Name = "Parth"
+  }
+}
