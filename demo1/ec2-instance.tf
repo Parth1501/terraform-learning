c5d06da651ca36632483e2a1b8e7f8302d1789cc
Parth Shethwala <37747371+Parth1501@users.noreply.github.com>
2021-06-18 12:28:38 +0530

    Update ec2-instance.tf

--- demo1/ec2-instance.tf
+++ demo1/ec2-instance.tf
@@ -1,24 +1,25 @@
+  
 terraform {
   required_providers {
-    aws={
-      source="hashicorp/aws"
-      version="~>3.27"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
     }
-      
   }
-  required_version=">=0.14.9
 
+  required_version = ">= 0.14.9"
 }
+
 provider "aws" {
-  profile="default"
-  region="us-west-2"
+  profile = "default"
+  region  = "us-west-2"
 }
 
 resource "aws_instance" "app_server" {
-  ami="ami-830c94e3"
-  instance_type="t2.micro"
-  
-  tag={
-    Name="Parth"
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Arjun"
   }
 }
