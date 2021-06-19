581fed3573d2d0cc832079018b3aadcc8f3e21bf
Parth Shethwala <shethwalaparth2000@gmail.com>
2021-06-19 11:01:23 +0530

    demo6

--- demo6/vpc.tf
+++ demo6/vpc.tf
@@ -22,7 +22,7 @@ resource "aws_subnet" "main-public-1" {
 }
 resource "aws_subnet" "main-public-2" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = "true"
   availability_zone       = "us-east-2b"
 
@@ -32,7 +32,7 @@ resource "aws_subnet" "main-public-2" {
 }
 resource "aws_subnet" "main-public-3" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.3.0/24"
   map_public_ip_on_launch = "true"
   availability_zone       = "us-east-2c"
 
@@ -53,7 +53,7 @@ resource "aws_subnet" "main-private-1" {
 }
 resource "aws_subnet" "main-private-2" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.4.0/24"
+  cidr_block              = "10.0.5.0/24"
   map_public_ip_on_launch = "false"
   availability_zone       = "us-east-2b"
 
@@ -63,7 +63,7 @@ resource "aws_subnet" "main-private-2" {
 }
 resource "aws_subnet" "main-private-3" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.4.0/24"
+  cidr_block              = "10.0.6.0/24"
   map_public_ip_on_launch = "false"
   availability_zone       = "us-east-2c"
 
