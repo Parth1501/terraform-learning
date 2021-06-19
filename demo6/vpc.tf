a1f746ce40095e69ba19bf9ad47e2a0931f0e775
Parth Shethwala <shethwalaparth2000@gmail.com>
2021-06-19 10:32:52 +0530

    demo6

--- demo6/vpc.tf
+++ demo6/vpc.tf
@@ -1,9 +1,9 @@
 resource "aws_vpc" "main" {
-    cidir_block="10.0.0.0/16"
+    cidr_block="10.0.0.0/16"
     instance_tenancy="default"
     enable_dns_support="true"
     enable_dns_hostnames="true"
-    enable_classic_link="false"
+    enable_classiclink="false"
     tags={
         Name="main"
     }
